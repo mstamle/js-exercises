efbd51d05c93cb4e9b9be4492cca6d1395619368
Tam Le <tam.le@let.local>
2019-09-04 14:50:44 +0200

    Practice Test Driven Development | WIP

--- JS/arrayOperations/arrayOperations.js
+++ JS/arrayOperations/arrayOperations.js
@@ -1,6 +1,6 @@
 /**
  * Perform reduce operation over an array
- * 
+ *
  * @template A
  * @template T
  * @param reduceStepFunction {(accumulator: A, element: T) => A}
@@ -10,23 +10,28 @@
  */
 
 function reduce(reduceStepFunction, initialAccumulator, array) {
-  TODO()
+  if(array) {return array.reduce(reduceStepFunction,initialAccumulator);
+  } else {
+    return false
+  }
 }
 
 /**
  * Calculate length of an array
- * 
+ *
  * @template T
  * @param array {T[]}
  * @returns {number}
  */
 function length(array) {
-  TODO()
+  return array.length;
 }
+
+const mapFunction = element => element + 1 
 
 /**
  * Return an array with mapFunction applied to all elements of given array
- * 
+ *
  * @template T
  * @template S
  * @param mapFunction {(element: T) => S}
@@ -34,19 +39,25 @@ function length(array) {
  * @returns {S[]}
  */
 function map(mapFunction, array) {
-  TODO()
+  return array.map(mapFunction);
 }
 
 /**
  * Removes elements that do not satisfy the predicate from array
- * 
+ *
  * @template T
  * @param predicate {(t: T) => boolean}
  * @array {T[]}
  * @returns {T[]}
  */
 function filter(predicate, array) {
-
+  let newArr = [];
+  array.forEach(el => {
+    if (predicate(el)) {
+      newArr.push(el);
+    }
+  });
+  return newArr;
 }
 
 /**
@@ -54,29 +65,39 @@ function filter(predicate, array) {
  */
 
 function isEmpty(array) {
-  TODO()
+  if (array.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
  * Appends an element to the end of the given array
  */
 function append(element, array) {
-  TODO()
+  array.push(element);
+  return array;
 }
 
 /**
  * Prepends an element to the start of the array
  */
 function prepend(element, array) {
-  TODO()
+  array.unshift(element)
+  return array
 }
 
 /**
  * Removes up to `n` elements from the start of the array
  * (returns an empty array if array.length <= n)
  */
-function drop(n, array) {
-  TODO()
+
+function drop(n, array) { // splice
+  for (i = 0; i < n; i++) {
+    array.shift();
+  }
+  return array;
 }
 
 /**
@@ -84,11 +105,15 @@ function drop(n, array) {
  * (returns an array if array.length <= n)
  */
 function take(n, array) {
-  TODO()
-}
-
-function TODO() {
-  throw "not implemented"
+  if (n >= array.length) {
+    return array.slice()
+  } else {
+    let newArr = []
+    for (i=0;i<=array.length;i++){
+      newArr.push[array[i]]
+    }
+    return newArr
+  }
 }
 
 module.exports = {
@@ -99,5 +124,6 @@ module.exports = {
   length,
   map,
   prepend,
-  reduce
-}
+  reduce,
+  take,
+};
--- JS/arrayOperations/arrayOperations.test.js
+++ JS/arrayOperations/arrayOperations.test.js
@@ -1,17 +1,43 @@
-const { isEmpty, map, reduce } = require('./arrayOperations')
+const { isEmpty, map, reduce, length, filter, append, drop, take } = require('./arrayOperations')
 
 describe('reduce', () => {
   it('should just return accumulator for an empty array', () => {
     expect(reduce((acc, item) => acc + 1, 0, [])).toBe(0)
   })
+  it('should  return number of elements', () => {
+    expect(reduce((acc, item) => acc + 1, 0, [1,2,3])).toBe(3)
+  })
+  it('should just return sum of the numbers', () => {
+    expect(reduce((acc, item) => acc + item, 0, [1,2,3])).toBe(6)
+  })
 })
+
+describe('return length', () => {
+  it('should just return length of an array', () => {
+    expect(length([])).toBe(0)
+  })
+ })
+
 
 describe('map', () => {
   it('should not change the structure of the array', () => {
     const array = [1,2,3,4,5]
     expect(map(element => element, array)).toEqual(array)
   })
+  it('should add 1', () => {
+    const array = [1,2,3,4,5]
+    const expectedResult = [2,3,4,5,6]
+    expect(map(element => element + 1, array)).toEqual(expectedResult)
+  })
 })
+
+describe('filter', () => {
+  it('returns filtered elements', () => {
+    const array = [1,2,3,4,5]
+    expect(filter(element => element <= 2, array)).toEqual([1, 2])
+  })
+ })
+
 
 describe('isEmpty', () => {
   it('should return true for an empty array', () => {
@@ -23,4 +49,25 @@ describe('isEmpty', () => {
       expect(isEmpty([element])).toBe(false)
     )
   });
+});
+describe('append', () => {
+  const array = [1, 2, 3, 4]
+  it('should return a new array with an appended element', () => {
+    expect(append(5, array)).toEqual([ 1, 2, 3, 4, 5])
+  })
+});
+describe('drop', () => {
+  const array = [1, 2, 3, 4]
+  it('should return a new array without dropped elements', () => {
+    expect(drop(2, array)).toEqual([3,4])
+  })
+});
+describe('take', () => {
+  const array = [1, 2, 3, 4]
+  it('should return a new array without dropped elements', () => {
+    expect(take(9, array)).toEqual(array)
+    expect(take(9, array)).toHaveLength(4)
+    expect(take(9, array)).not.toBe(array)
+    
+  })
 });
